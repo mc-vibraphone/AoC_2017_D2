cbdd190258b4c657ef125d6ec0a6346c745a399b
mc_vibraphone <tom.mcmanus@gmail.com>
2017-12-11 19:17:52 -0500

    Impl'm'n for D2.

--- aoc_checksum.py
+++ aoc_checksum.py
@@ -1,16 +1,43 @@
-import unittest
-import aoc_checksum
+import io
 
-results = [{'input':'''5 1 9 5 7 5 3 2 4 6 8''', 'output':18},
-            ]
-class CheckSumTest(unittest.TestCase):
-    def test_results(self):
-        print('results are {}'.format(results))
-        for r in results:
-            print('Checking {} checksums {}'.format(r['input'], r['offset'], r['output']))
-            self.assertEqual(aoc_checksum.checksum(r['input'], r['output']))
+
+def aoc_checksum(input):
+    checksum = 0
+    for _l in io.StringIO(input):
+        _l = _l.strip()
+        if(_l):
+            nums = list(map(int, _l.split()))
+            checksum += max(nums) - min(nums)
+    return checksum
+
+def get_checksum2(nums):
+    ''' Looks through a list of ints and finds the first set that are evenly divisible'''
+    if len(nums) >= 2:
+        nums = sorted(nums)
+        print('Looking at {}'.format(nums))
+        this_num = nums[0]
+        for x in nums[1:]:
+            if x % this_num == 0:
+                print('Found {} / {} is {}'.format(x, this_num, x / this_num))
+                return x / this_num
+        return get_checksum2(nums[1:])
+    else:
+        return 0
+   
+def aoc_checksum_2(input):
+    checksum = 0
+    for _l in io.StringIO(input):
+        _l = _l.strip()
+        if(_l):
+            nums = list(map(int, _l.split()))
+            checksum += get_checksum2(nums)
+    return checksum
+
 
 if __name__ == '__main__':
     print('hello world')
-    unittest.main()
-
+    with open('./test_input', 'r') as f:
+        test_input = f.read()
+    print('Input is\n{}\n'.format(''.join('{}'.format(l) for l in test_input)))
+    print('Checksum of input is {}'.format(aoc_checksum(test_input)))
+    print('Checksum2 of input is {}'.format(aoc_checksum_2(test_input)))
--- test_d2.py
+++ test_d2.py
@@ -1,14 +1,29 @@
 import unittest
 import aoc_checksum
 
-results = [{'input':'''5 1 9 5 7 5 3 2 4 6 8''', 'output':18},
+results = [{'input':'''5 1 9 5
+    7 5 3 
+    2 4 6 8
+    ''', 'output':18},
             ]
+results_2 = [{'input':'''5 9 2 8
+    9 4 7 3
+    3 8 6 5
+    ''', 'output':9},
+    ]
+
 class CheckSumTest(unittest.TestCase):
     def test_results(self):
         print('results are {}'.format(results))
         for r in results:
-            print('Checking {} checksums {}'.format(r['input'], r['offset'], r['output']))
-            self.assertEqual(aoc_checksum.checksum(r['input'], r['output']))
+            print('Checking \n[{}]\n checksums {}'.format(r['input'], r['output']))
+            self.assertEqual(aoc_checksum.aoc_checksum(r['input']), r['output'])
+
+    def test_chksm_2(self):
+        print('results are {}'.format(results_2))
+        for r in results_2:
+            print('Checking \n[{}]\n checksums {}'.format(r['input'], r['output']))
+            self.assertEqual(aoc_checksum.aoc_checksum_2(r['input']), r['output'])
 
 if __name__ == '__main__':
     print('hello world')
